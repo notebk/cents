11ccf4e2c7046fe35bde57872acc4c6db589302b
notebk <egzz93@me.com>
2018-08-30 21:42:04 -0400

    added from apos-samples key lib features and boilerplate setup

--- app.js
+++ app.js
@@ -16,25 +16,55 @@ var apos = require('apostrophe')({
     
     // However any modules that are not present by default in Apostrophe must at
     // least have a minimal configuration here: `moduleName: {}`
-    
-    // This configures our default page template
-    'apostrophe-pages': {
-      types: [
-        {
-          name: 'default',
-          label: 'Default'
-        },
-        {
-          name: 'home',
-          label: 'Home'
-        },
-        
-      ]
-    },
+
     // If a template is not found somewhere else, serve it from the top-level
     // `views/` folder of the project
 
-    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
+    'apostrophe-templates': {
+      viewsFolderFallback: path.join(__dirname, 'views') 
+      // See also lib/modules/apostrophe-templates/index.js
+    },
+  
+    // see lib/modules/one-column-widgets/index.js, et cetera 
+    // Use index.js files for each module to keep app.js readable
 
+    'one-column-widgets': {},
+    'two-column-widgets': {},
+    'three-column-widgets': {},
+    'products': {},
+    'products-pages': {},
+    // People who specialize in various products
+    'specialists': {},
+    'specialists-pages': {},
+    'theme': {},
+    'test-widgets': {},
+    'apostrophe-global': {
+      addFields: [
+        {
+          name: 'dealerRecommendedConfig',
+          type: 'checkboxes',
+          label: 'Recommended Dealer config',
+          choices: [
+            {
+              label: 'Is certified',
+              value: 'isCertified'
+            },
+            {
+              label: 'Is prefered',
+              value: 'isPreferredDealer'
+            },
+            {
+              label: 'Best chance in stock',
+              value: 'isBestChanceInStock'
+            },
+            {
+              label: 'Has a network',
+              value: 'networkName'
+            }
+          ]
+        }  
+      ]
+    }
   }
 });
+
